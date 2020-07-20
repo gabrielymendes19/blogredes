9ec22683dd9d448cd59526d5043b9e7d1fd838a4
ericserka <ericserka@hotmail.com>
2020-07-20 07:18:28 -0300

    comecando paginacao no front

--- index.php
+++ index.php
@@ -242,6 +242,110 @@
                 }
                 ?>
             </ul>
+
+            <!--inicio botoes paginacao-->
+            <style>
+              <?php
+                  if(isset($_GET['pg'])) {
+                      $num_pg = $_GET['pg'];
+                  }
+                  else {
+                      $num_pg = 1;
+                  }
+              ?>
+              .paginas a.ativo<?php echo $num_pg; ?> {
+                  background: red;
+                  color: white;
+              }
+            </style>
+            <?php
+                if (empty($_GET['pg'])) {
+
+                }
+                else {
+                    $pg = $_GET['pg'];
+                    if (!is_numeric($pg)) {
+                        echo '<script language="JavaScript">location.href="home.php?acao=ver-postagens";</script>';
+                    }
+                }
+                if (isset($pg)) {
+                    $pg = $_GET['pg'];
+                }
+                else {
+                    $pg = 1;
+                }
+                $quantidade = 3;
+                $inicio = ($pg*$quantidade) - $quantidade;
+                $sql = "SELECT * from posts ORDER BY id DESC LIMIT $inicio,$quantidade";
+                try {
+                    $result = $conexao->prepare($sql);
+                    $result->execute();
+                    $totalRegistros = $result->rowCount();
+                }
+                catch (PDOException $e) {
+                    echo $e;
+                }
+                if ($totalRegistros <= $quantidade) {
+                    
+                }
+                else {
+                    $paginas = ceil($totalRegistros/$quantidade);
+                    if ($pg > $paginas) {
+                        echo '<script language="JavaScript">location.href="home.php?acao=ver-postagens";</script>';
+                    }
+                    $links = 5;
+                    if (isset($i)) {
+
+                    }
+                    else {
+                        $i = '1';
+                    }
+            ?>
+
+            <div class="paginas">
+              <a href="home.php?acao=ver-postagens&pg=1">Primeira Página</a>
+              <?php
+                  if (isset($_GET['pg'])) {
+                      $num_pg = $_GET['pg'];
+                  }
+                  for ($i = $pg - $links; $i <= $pg - 1 ; $i++) {
+                      if ($i <= 0) {
+
+                      }
+                      else {
+              ?>
+              <a href="home.php?acao=ver-postagens&pg=<?php echo $i; ?>" class="ativo<?php echo $i; ?>"><?php echo $i; ?></a>
+              <?php
+                      }
+                  }
+              ?>
+              <a href="home.php?acao=ver-postagens&pg=<?php echo $pg; ?>" class="ativo<?php echo $i; ?>"><?php echo $pg; ?></a>
+
+              <?php
+                  for($i = $pg + 1 ; $i <= $pg + $links ; $i++) {
+                      if($i > $paginas) {
+
+                      }
+                      else {
+              ?>
+
+              <a href="home.php?acao=ver-postagens&pg=<?php echo $i; ?>" class="ativo<?php echo $i; ?>"><?php echo $i; ?></a>
+
+              <?php            
+
+                      }
+                  }
+              ?>
+
+              <a href="home.php?acao=ver-postagens&pg=<?php echo $paginas; ?>">Última Página</a>  
+
+      </div> <!--paginas-->
+      
+      <?php
+          }
+
+      ?>
+      <!--fim botoes paginacao-->
           </div>
           <!-- /.divcenter -->
 
